d939e0ce13a3f74dd325a7310342de9417daa6fb
pipo <pipo@localhost.localdomain>
2014-04-26 01:02:33 +0900

    angularjs : AjaxTest

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-Masa's Free Space
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Masa's Free Space</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	
+	<body>
+		<h1>Masa's Free Space</h1>
+		<nav>
+			<div><a href="js/angularjs/ajaxPostBasic/">Form Post ( Angularjs + PHP )</a></div>
+			<div><a href="js/angularjs/ajaxPostFileUpload/">Form FileUpload And Post ( Angularjs + PHP )</a></div>
+		</nav>
+	</body>
+</html>
